8707c0daf62bcb77b95863030164149f4a19c0dd
ZaidShaikh <szaid5146@gmail.com>
2026-07-20 08:38:25 +0530

    Async/Await in useEffect

--- Mini Projects/crypto-dash/src/App.jsx
+++ Mini Projects/crypto-dash/src/App.jsx
@@ -8,20 +8,35 @@ const App = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((res) => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL);
         if (!res.ok) throw new Error('Failed to fetch data');
-        return res.json();
-      })
-      .then((data) => {
+        const data = await res.json();
         console.log(data);
         setCoins(data);
         setLoading(false);
-      })
-      .catch((err) => {
+      } catch (error) {
         setError(err.message);
         setLoading(false);
-      });
+      }
+    };
+    fetchCoins();
+
+    // fetch(API_URL)
+    //   .then((res) => {
+    //     if (!res.ok) throw new Error('Failed to fetch data');
+    //     return res.json();
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //     setCoins(data);
+    //     setLoading(false);
+    //   })
+    //   .catch((err) => {
+    //     setError(err.message);
+    //     setLoading(false);
+    //   });
   }, []);
 
   return (
