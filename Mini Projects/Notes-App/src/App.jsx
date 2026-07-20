4e3e70ce593dfb32f3830ac806ac5d0a4e5cc618
ZaidShaikh <szaid5146@gmail.com>
2026-07-20 07:01:33 +0530

    Side Effect

--- Mini Projects/Notes-App/src/App.jsx
+++ Mini Projects/Notes-App/src/App.jsx
@@ -1,17 +1,32 @@
 import NoteForm from './components/note-form';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NoteList from './components/noteList';
 
 const App = () => {
   // Global state
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(() => {
+    const notes = JSON.parse(localStorage.getItem('notes'));
+    return notes || [];
+  });
+
+  // This function runs when any note is update,this is component updated method
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
+
+  const deleteNote = (id) => {
+    const confirmDelete = window.confirm('Are your sure you wanna do this?');
+    if (confirmDelete) {
+      setNotes(notes.filter((note) => note.id !== id));
+    }
+  };
 
   return (
     <div className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg'>
       <h2 className='text-2xl font-bold mb-4 text-center'>📝 Notes App</h2>
 
       <NoteForm notes={notes} setNotes={setNotes} />
-      <NoteList notes={notes} />
+      <NoteList notes={notes} deleteNote={deleteNote} />
     </div>
   );
 };
