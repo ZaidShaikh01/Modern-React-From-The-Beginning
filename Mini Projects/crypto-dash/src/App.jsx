7a6ce1a70b416ef1cf73cdac01b5f5ae36ba61b9
ZaidShaikh <szaid5146@gmail.com>
2026-07-21 06:59:43 +0530

    Filter Coins

--- Mini Projects/crypto-dash/src/App.jsx
+++ Mini Projects/crypto-dash/src/App.jsx
@@ -1,17 +1,21 @@
 import { useState, useEffect } from 'react';
 import CoinCard from './component/CoinCard';
+import LimitSelector from './component/LimitSelector';
+import FilterInput from './component/FilterInput';
 const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     const fetchCoins = async () => {
       try {
         const res = await fetch(
-          `${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`,
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`,
         );
         if (!res.ok) throw new Error('Failed to fetch data');
         const data = await res.json();
@@ -40,7 +44,14 @@ const App = () => {
     //     setError(err.message);
     //     setLoading(false);
     //   });
-  }, []);
+  }, [limit]);
+
+  const filteredCoins = coins.filter((coin) => {
+    return (
+      coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+      coin.symbol.toLowerCase().includes(filter.toLowerCase())
+    );
+  });
 
   return (
     <div>
@@ -48,9 +59,13 @@ const App = () => {
       {loading && <p>Loading...</p>}
       {error && <div className='error'>{error}</div>}
 
+      <div className='top-controls'>
+        <FilterInput filter={filter} onFilterChange={setFilter} />
+        <LimitSelector limit={limit} onLimitChange={setLimit} />
+      </div>
       {!loading && !error && (
         <main className='grid'>
-          {coins.map((coin) => (
+          {filteredCoins.map((coin) => (
             <CoinCard key={coin.id} coin={coin} />
           ))}
         </main>
