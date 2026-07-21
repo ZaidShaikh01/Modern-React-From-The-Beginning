7054ef53311ff7cf98d9b01a93cd13cead75834b
ZaidShaikh <szaid5146@gmail.com>
2026-07-21 07:55:37 +0530

    Not found page added

---
+++ Mini Projects/crypto-dash/src/pages/about.jsx
@@ -0,0 +1,33 @@
+const AboutPage = () => {
+    return ( 
+        <div className="about">
+            <h1>About Crypto Dash</h1>
+            <p>
+                The project is built as part of a React tutorial to help you understand hooks, components, state managment, and integrating with external APIs.
+            </p>
+            <p>
+                The project is built as part of a React tutorial to help you understand hooks, components, state managment, and integrating with external APIs.
+            </p>
+            <p>
+                The project is built as part of a React tutorial to help you understand hooks, components, state managment, and integrating with external APIs.
+            </p>
+            <p>
+                The project is built as part of a React tutorial to help you understand hooks, components, state managment, and integrating with external APIs.
+            </p>
+            <p>
+                The project is built as part of a React tutorial to help you understand hooks, components, state managment, and integrating with external APIs.
+            </p>
+            <p>
+                The project is built as part of a React tutorial to help you understand hooks, components, state managment, and integrating with external APIs.
+            </p>
+            <p>
+                The project is built as part of a React tutorial to help you understand hooks, components, state managment, and integrating with external APIs.
+            </p>
+            <p>
+                The project is built as part of a React tutorial to help you understand hooks, components, state managment, and integrating with external APIs.
+            </p>
+        </div>
+     );
+}
+ 
+export default AboutPage;
