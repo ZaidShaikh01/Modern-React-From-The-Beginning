f6f5da36d3a7ac59994421b3bb91f08285cfdc4a
ZaidShaikh <szaid5146@gmail.com>
2026-07-24 08:04:22 +0530

    Mobile HamBurger menu added

--- Mini Projects/friendly-dev/app/components/Navbar.tsx
+++ Mini Projects/friendly-dev/app/components/Navbar.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router';
-import { FaLaptopCode } from 'react-icons/fa';
+import { FaLaptopCode, FaTimes, FaBars } from 'react-icons/fa';
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const base = 'transition hover:text-blue-400';
   const active = 'text-blue-400 font-semibold';
   return (
@@ -15,6 +18,7 @@ const Navbar = () => {
         </NavLink>
 
         {/* Dekstop nav */}
+        {/* By adding hidden and md:flex it is visible on dekstop */}
         <div className='hidden md:flex ietms-center gap-6'>
           <div className='space-x-4 text-sm text-gray-300'>
             <NavLink
@@ -49,7 +53,67 @@ const Navbar = () => {
             </NavLink>
           </div>
         </div>
+        {/* Mobile Hamburger */}
+        {/* By adding hidden on md it is not visible on dekstop */}
+        <div className='md:hidden flex items-center gap-4' title='Menu'>
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className='text-blue-400 text-xl cursor-pointer'
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
       </div>
+      {/* Mobile Nav */}
+      {menuOpen && (
+        <div className='bg-gray-800 border-t border-gray-700 px-6 py-4 space-x-4 space-y-2 text-center md:hidden'>
+          <NavLink
+            className={({ isActive }) => (isActive ? active : base)}
+            to='/'
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? active : base)}
+            to='/projects'
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? active : base)}
+            to='/about'
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          >
+            About
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? active : base)}
+            to='/blog'
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          >
+            Blog
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? active : base)}
+            to='/contact'
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          >
+            Contact
+          </NavLink>
+        </div>
+      )}
     </nav>
   );
 };
