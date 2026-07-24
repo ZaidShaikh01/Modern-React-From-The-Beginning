5b9e0106316c2b7451389febf6628da7aafcc121
ZaidShaikh <szaid5146@gmail.com>
2026-07-24 07:46:13 +0530

    Friendly Devloper initialized

---
+++ Mini Projects/friendly-dev/app/routes/contact/index.tsx
@@ -0,0 +1,7 @@
+const ContactPage = () => {
+    return ( <section>
+        <h2 className="text-3xl font-bold text-white mb-8 text-center">Contact page</h2>
+    </section> );
+}
+ 
+export default ContactPage;
---
+++ Mini Projects/friendly-dev/app/routes/home/index.tsx
@@ -0,0 +1,12 @@
+import type { Route } from './+types/index';
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'The Friendly Dev' },
+    { name: 'description', content: 'Custom Website Devlopment' },
+  ];
+}
+
+export default function Home() {
+  return <section>My App</section>;
+}
