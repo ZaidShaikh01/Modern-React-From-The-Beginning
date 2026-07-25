cbea686c30cb5854433b5ef55587e6b9632e4043
ZaidShaikh <szaid5146@gmail.com>
2026-07-25 07:57:44 +0530

    Project type

--- Mini Projects/friendly-dev/app/routes/projects/index.tsx
+++ Mini Projects/friendly-dev/app/routes/projects/index.tsx
@@ -1,15 +1,18 @@
 import type { Route } from './+types/index';
+import type { Project } from '~/types';
 
 // Request has the type of Route.LoaderArgs & Promise is the return type of the function
-export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
   const res = await fetch('http://localhost:8000/projects');
   const data = await res.json();
   return { projects: data };
 }
 
-const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
-  const {projects} = loaderData;
-  console.log(projects)
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData as { projects: Project[] };
+  console.log(projects);
 
   return (
     <>
