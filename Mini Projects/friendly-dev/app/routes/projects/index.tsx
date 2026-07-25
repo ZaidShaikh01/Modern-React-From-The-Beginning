c59014b257ecd7846d0df5d9b100239bbb97c37a
ZaidShaikh <szaid5146@gmail.com>
2026-07-25 08:17:02 +0530

    Display Loader Data

--- Mini Projects/friendly-dev/app/routes/projects/index.tsx
+++ Mini Projects/friendly-dev/app/routes/projects/index.tsx
@@ -1,3 +1,4 @@
+import ProjectCard from '~/components/ProjectCard';
 import type { Route } from './+types/index';
 import type { Project } from '~/types';
 
@@ -7,16 +8,25 @@ export async function loader({
 }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
   const res = await fetch('http://localhost:8000/projects');
   const data = await res.json();
+
+  // Projects is an object of data. & data is list of type Project & we have described project in type.ts
+
   return { projects: data };
 }
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  // console.log(loaderData);
+  // We are destructuring the data from loaderData
   const { projects } = loaderData as { projects: Project[] };
-  console.log(projects);
 
   return (
     <>
       <h2 className='text-3xl text-white font-bold mb-8'>Projects</h2>
+      <div className='grid gap-6 sm:grid-cols-2'>
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </>
   );
 };
