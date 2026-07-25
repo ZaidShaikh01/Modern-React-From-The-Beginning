9715a57d2aecbe79200e57efe8826165c028643f
ZaidShaikh <szaid5146@gmail.com>
2026-07-25 07:51:42 +0530

    Fetch Projects With Loader

--- Mini Projects/friendly-dev/app/routes/projects/index.tsx
+++ Mini Projects/friendly-dev/app/routes/projects/index.tsx
@@ -1,4 +1,16 @@
-const ProjectsPage = () => {
+import type { Route } from './+types/index';
+
+// Request has the type of Route.LoaderArgs & Promise is the return type of the function
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+  const res = await fetch('http://localhost:8000/projects');
+  const data = await res.json();
+  return { projects: data };
+}
+
+const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
+  const {projects} = loaderData;
+  console.log(projects)
+
   return (
     <>
       <h2 className='text-3xl text-white font-bold mb-8'>Projects</h2>
