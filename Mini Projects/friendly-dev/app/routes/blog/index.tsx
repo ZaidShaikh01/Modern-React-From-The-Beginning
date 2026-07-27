aa07690b019d304a4a8158dcb17d2e9d1f05a502
ZaidShaikh <szaid5146@gmail.com>
2026-07-27 12:45:05 +0530

    Display Blog

--- Mini Projects/friendly-dev/app/routes/blog/index.tsx
+++ Mini Projects/friendly-dev/app/routes/blog/index.tsx
@@ -1,8 +1,10 @@
 import type { Route } from './+types';
 import type { PostMeta } from '~/types';
-import { Link } from 'react-router';
+import PostCard from '~/components/PostCard';
 
-export async function loader({ request }: Route.LoaderArgs): Promise<{posts:PostMeta[]}> {
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
   const url = new URL('/posts-meta.json', request.url);
   const res = await fetch(url.href);
   if (!res.ok) throw new Error('Failed to fetch data');
@@ -12,11 +14,14 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{posts:Post
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
   const { posts } = loaderData;
-  console.log(posts);
+
   return (
-    <>
+    <div className='max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900'>
       <h2 className='text-3xl text-white font-bold mb-8'>Blog</h2>
-    </>
+      {posts.map((post) => (
+        <PostCard post={post} key={post.slug} />
+      ))}
+    </div>
   );
 };
 
