597871d97694c2d5e2043e1ce3483c93666ca448
ZaidShaikh <szaid5146@gmail.com>
2026-07-27 11:41:19 +0530

    React Router Action Example

--- Mini Projects/friendly-dev/app/routes/contact/index.tsx
+++ Mini Projects/friendly-dev/app/routes/contact/index.tsx
@@ -1,7 +1,97 @@
-const ContactPage = () => {
-    return ( <>
-        <h2 className="text-3xl font-bold text-white mb-8 text-center">Contact page</h2>
-    </> );
+import type { Route } from './+types';
+import { Form } from 'react-router';
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const name = formData.get('name');
+  const email = formData.get('email');
+  const subject = formData.get('subject');
+  const message = formData.get('message');
+  const data = {
+    name,
+    email,
+    subject,
+    message,
+  };
+  return { message: 'Form submitted successfully', data };
 }
- 
+const ContactPage = ({ actionData }: Route.ComponentProps) => {
+  return (
+    <div className='max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900 rounded text-center'>
+      <h2 className='text-3xl font-bold text-white mb-8 text-center'>
+        Contact page
+      </h2>
+      {actionData?.message ? (
+        <p className='mb-6 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-md'>
+          {actionData.message}
+        </p>
+      ) : null}
+      <Form method='post' className='space-y-6'>
+        <div>
+          <label
+            htmlFor='name'
+            className='block text-sm font-medium text-gray-300'
+          >
+            Full Name
+          </label>
+
+          <input
+            type='text'
+            id='name'
+            name='name'
+            className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='email'
+            className='block text-sm font-medium text-gray-300'
+          >
+            Email
+          </label>
+
+          <input
+            type='email'
+            id='email'
+            name='email'
+            className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='subject'
+            className='block text-sm font-medium text-gray-300'
+          >
+            Subject
+          </label>
+
+          <input
+            type='text'
+            id='subject'
+            name='subject'
+            className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='message'
+            className='block text-sm font-medium text-gray-300'
+          >
+            Message
+          </label>
+
+          <textarea
+            id='message'
+            name='message'
+            className='w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100'
+          />
+        </div>
+        <button className='w-full cursor-pointer text-white p-2 rounded-lg hover:bg-blue-700 bg-blue-600'>
+          Send Message
+        </button>
+      </Form>
+    </div>
+  );
+};
+
 export default ContactPage;
