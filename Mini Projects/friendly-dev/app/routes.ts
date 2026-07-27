5df623a2ce7f6d3a1dfcf0668856e34d1b0e8821
ZaidShaikh <szaid5146@gmail.com>
2026-07-27 13:01:38 +0530

    Single Post & Markdown

--- Mini Projects/friendly-dev/app/routes.ts
+++ Mini Projects/friendly-dev/app/routes.ts
@@ -16,6 +16,7 @@ export default [
     route('projects', './routes/projects/index.tsx'),
     route('projects/:id', './routes/projects/details.tsx'),
     route('blog', './routes/blog/index.tsx'),
+    route('blog/:slug', './routes/blog/details.tsx'),
     route('*', './routes/errros/not-found.tsx'),
   ]),
 ] satisfies RouteConfig;
