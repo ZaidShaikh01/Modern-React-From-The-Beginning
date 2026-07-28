e447ef5998d60809b4b82201724c61bf107e9fe0
ZaidShaikh <szaid5146@gmail.com>
2026-07-28 08:19:26 +0530

    Latest Blog posts

--- Mini Projects/friendly-dev/app/routes/blog/index.tsx
+++ Mini Projects/friendly-dev/app/routes/blog/index.tsx
@@ -3,6 +3,7 @@ import type { Route } from './+types';
 import type { PostMeta } from '~/types';
 import PostCard from '~/components/PostCard';
 import Pagination from '~/components/Pagination';
+import PostFilter from '~/components/PostFilter';
 
 export async function loader({
   request,
@@ -20,10 +21,18 @@ export async function loader({
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
   // This is for pagination
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
   const blogsPerPage = 2;
 
   const { posts } = loaderData;
-
+  // Filtered posts
+  const filteredPost = posts.filter((post) => {
+    const query = searchQuery.toLowerCase();
+    return (
+      post.title.toLowerCase().includes(query) ||
+      post.excerpt.toLowerCase().includes(query)
+    );
+  });
   // Calculate total pages
   const totalPages = Math.ceil(posts.length / blogsPerPage);
 
@@ -32,12 +41,22 @@ const BlogPage = ({ loaderData }: Route.ComponentProps) => {
   // Index of first blog on page
   const indexOfFirst = indexOfLast - blogsPerPage;
   // Get Current blogs of that page
-  const currentBlogs = posts.slice(indexOfFirst, indexOfLast);
+  const currentBlogs = filteredPost.slice(indexOfFirst, indexOfLast);
 
   return (
     <>
       <div className='max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900'>
         <h2 className='text-3xl text-white font-bold mb-8'>Blog</h2>
+        <PostFilter
+          onSearchChange={(query) => {
+            setSearchQuery(query);
+            setCurrentPage(1);
+          }}
+          searchQuery={searchQuery}
+        />
+        {currentBlogs.length === 0 && (
+          <p className='text-gray-500 text-center'>No Such Blog unfortunately 😔</p>
+        )}
         {currentBlogs.map((post) => (
           <PostCard post={post} key={post.slug} />
         ))}
--- Mini Projects/friendly-dev/app/routes/home/index.tsx
+++ Mini Projects/friendly-dev/app/routes/home/index.tsx
@@ -2,6 +2,8 @@ import FeaturedProjects from '~/components/FeaturedProjects';
 import type { Route } from './+types/index';
 import AboutPreview from '~/components/preview';
 import type { Project } from '~/types';
+import type { PostMeta } from '~/types';
+import LatestBlogPost from '~/components/LatestBlogPost';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,18 +14,30 @@ export function meta({}: Route.MetaArgs) {
 
 export async function loader({
   request,
-}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
-  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-  const data = await res.json();
-  return { projects: data };
+}: Route.LoaderArgs): Promise<{ projects: Project[]; posts: PostMeta[] }> {
+  const url = new URL(request.url);
+  // Promise.all is used here to get the values concurrently
+  const [projectRes, PostRes] = await Promise.all([
+    fetch(`${import.meta.env.VITE_API_URL}/projects`),
+    fetch(new URL('/posts-meta.json', url)),
+  ]);
+  if (!projectRes.ok || !PostRes.ok)
+    throw new Error('Error While Fetching The Data....');
+  const [projects, posts] = await Promise.all([
+    projectRes.json(),
+    PostRes.json(),
+  ]);
+
+  return { projects: projects, posts: posts };
 }
 
 const HomePage = ({ loaderData }: Route.ComponentProps) => {
-  const {projects} = loaderData;
-  
+  const { projects, posts } = loaderData;
+
   return (
     <>
-      <FeaturedProjects projects={projects} count={2}/>
+      <FeaturedProjects projects={projects} count={2} />
+      <LatestBlogPost limit={3} posts={posts} />
       <AboutPreview />
     </>
   );
