a258637ac1db6342395dbaa50450744b7497cd74
ZaidShaikh <szaid5146@gmail.com>
2026-07-31 09:02:14 +0530

    Show User Suggestions

--- Mini Projects/github-finder/src/api/github.ts
+++ Mini Projects/github-finder/src/api/github.ts
@@ -1,10 +1,23 @@
-export const fetchGithubUser = async (username: string) =>{
-     const res = await fetch(
-        `${import.meta.env.VITE_GITHUB_API_URL}/users/${username}`,
-      );
-      if (!res.ok) throw new Error('Error while fetching the data');
+export const fetchGithubUser = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/users/${username}`,
+  );
+  if (!res.ok) throw new Error('Error while fetching the data');
 
-      const data = await res.json();
-      
-      return data;
-}
+  const data = await res.json();
+
+  return data;
+};
+
+export const searchGithubUser = async (query: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/search/users/?q=${query}`,
+  );
+  if (!res.ok) throw new Error('Error while fetching the data');
+
+  const data = await res.json();
+
+  // Data is object and it has array call items
+
+  return data.items;
+};
--- Mini Projects/github-finder/src/components/UserSearch.tsx
+++ Mini Projects/github-finder/src/components/UserSearch.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { fetchGithubUser } from '../api/github';
+import { fetchGithubUser, searchGithubUser } from '../api/github';
+import { useDebounce } from 'use-debounce';
 import UserCard from './UserCard';
-import { FaClock, FaUser } from 'react-icons/fa';
 import RecentSearches from './RecentSearch';
+import type { GitHubUser } from '../type';
 
 const UserSearch = () => {
   const [userName, setUserName] = useState('');
@@ -14,8 +15,13 @@ const UserSearch = () => {
     return stored ? JSON.parse(stored) : [];
   });
 
+  // So in this it is taking 2 parmeters, one is userName and secound is ms after which the fuction is fired off
+  const [debouncedUsername] = useDebounce(userName, 300);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
   // We dont need to initialize this value as we do in useEffect 'cuz useQuery takes care of it
-  const { data, isLoading, isError, error } = useQuery({
+  // Query to fetch specific user
+  const { data, isLoading, isError, error, refetch } = useQuery({
     // This is like the bracket we use in useEffect, everytime this changes it will call this funcion again, like listening to this key
 
     // The whole array is like a composite key, it works like this
@@ -52,11 +58,22 @@ const UserSearch = () => {
     enabled: !!submittedUserName, // What !! does is that it will turn the submittedUserName in the boolean value.
   });
 
+  // Query to fetch suggestions for search users
+  // We are renaming the data to suggestions
+  const { data: suggestions } = useQuery({
+    queryKey: ['github-user-suggestions', debouncedUsername],
+    // & this is where you write the function
+    queryFn: () => searchGithubUser(debouncedUsername),
+    // This is will prevent submitting for the first time
+    enabled: debouncedUsername.length > 1, // What !! does is that it will turn the submittedUserName in the boolean value.
+  });
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const trimmed = userName.trim();
     if (!trimmed) return;
     setSubmittedUserName(trimmed);
+    setUserName('');
 
     // We are taking previous user list, then updating it, if user already exists, then we dont add it again by removing the first occurance
     setRecentUsers((prev) => {
@@ -70,15 +87,48 @@ const UserSearch = () => {
     // JSON.stringify converts the string into the Json Object
     localStorage.setItem('recentUsers', JSON.stringify(recentUsers));
   }, [recentUsers]);
+
   return (
     <>
       <form onSubmit={handleSubmit} className='form'>
-        <input
-          type='text'
-          placeholder='Enter GitHub UserName...'
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
+        <div className='dropdown-wrapper'>
+          <input
+            type='text'
+            placeholder='Enter GitHub UserName...'
+            value={userName}
+            onChange={(e) => {
+              const val = e.target.value;
+              setUserName(e.target.value);
+              setShowSuggestions(val.trim().length > 1);
+            }}
+          />
+
+          {showSuggestions && suggestions?.length > 0 && (
+            <ul className='suggestions'>
+              {suggestions.slice(0, 5).map((user: GitHubUser) => (
+                <li
+                  key={user.login}
+                  onClick={() => {
+                    setUserName(user.login);
+                    setShowSuggestions(false);
+                    if (submittedUserName !== user.login) {
+                      setSubmittedUserName(user.login);
+                    } else {
+                      refetch();
+                    }
+                  }}
+                >
+                  <img
+                    src={user.avatar_url}
+                    alt={user.name}
+                    className='avatar-xs'
+                  />
+                  {user.login}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
         <button type='submit'>Search</button>
       </form>
       {isLoading && <p className='status'>Loading...</p>}
