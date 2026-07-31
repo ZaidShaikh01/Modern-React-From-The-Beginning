5f379678da169373a291ce16ff8d62d33f2d7752
ZaidShaikh <szaid5146@gmail.com>
2026-07-31 08:14:54 +0530

    Recent Searches Feature

--- Mini Projects/github-finder/src/components/UserSearch.tsx
+++ Mini Projects/github-finder/src/components/UserSearch.tsx
@@ -3,10 +3,13 @@ import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { fetchGithubUser } from '../api/github';
 import UserCard from './UserCard';
+import { FaClock, FaUser } from 'react-icons/fa';
+import RecentSearches from './RecentSearch';
 
 const UserSearch = () => {
   const [userName, setUserName] = useState('');
   const [submittedUserName, setSubmittedUserName] = useState('');
+  const [recentUsers, setRecentUsers] = useState<string[]>([]);
 
   // We dont need to initialize this value as we do in useEffect 'cuz useQuery takes care of it
   const { data, isLoading, isError, error } = useQuery({
@@ -20,7 +23,16 @@ const UserSearch = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setSubmittedUserName(userName.trim());
+    const trimmed = userName.trim();
+    if (!trimmed) return;
+    setSubmittedUserName(trimmed);
+
+    // We are taking previous user list, then updating it, if user already exists, then we dont add it again by removing the first occurance
+    setRecentUsers((prev) => {
+      const updated = [trimmed, ...prev.filter((user) => user !== trimmed)];
+      // returning the last 5 values
+      return updated.slice(0, 5);
+    });
   };
 
   return (
@@ -33,13 +45,18 @@ const UserSearch = () => {
           onChange={(e) => setUserName(e.target.value)}
         />
         <button type='submit'>Search</button>
-        {isLoading && <p className='status'>Loading...</p>}
-        {isError && <p className='status error'>{error.message}</p>}
-
-        {data && (
-          <UserCard data={data} />
-        )}
       </form>
+      {isLoading && <p className='status'>Loading...</p>}
+      {isError && <p className='status error'>{error.message}</p>}
+      {data && <UserCard data={data} />}
+      {recentUsers.length > 0 && (
+        <RecentSearches
+          users={recentUsers}
+          onSelect={(userName) => {
+            (setUserName(userName), setSubmittedUserName(userName));
+          }}
+        />
+      )}
     </>
   );
 };
